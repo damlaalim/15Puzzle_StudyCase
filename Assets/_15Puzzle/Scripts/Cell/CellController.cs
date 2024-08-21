9facb970ed39aa11569ad23b1b646dd392aea2c5
damlaalim <damlaalim81@gmail.com>
2024-08-21 18:55:02 +0300

    - added verification of the position of cells
    - added fake shuffle animations of cells

--- Assets/_15Puzzle/Scripts/Cell/CellController.cs
+++ Assets/_15Puzzle/Scripts/Cell/CellController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
+using _15Puzzle.Scripts.Level;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _15Puzzle.Scripts.Cell
 {
@@ -13,6 +16,8 @@ namespace _15Puzzle.Scripts.Cell
         [SerializeField] private float _swipeTime;
         [SerializeField] private AnimationCurve _curve;
         [SerializeField] private TextMeshProUGUI _numText;
+        [SerializeField] private LevelManager _levelManager;
+        [SerializeField] private CellManager _cellManager;
 
         public void ChangeNumberText(string text) => _numText.text = text;
         
@@ -24,24 +29,39 @@ namespace _15Puzzle.Scripts.Cell
             gridPosition = new Vector2Int(gridPosition.x + (int)direction.x, gridPosition.y + (int)direction.y);
 
             StartCoroutine(Swipe_Routine(targetPos));
-        }
+            var correctPos = CalculateCorrectPosition();
 
-        private IEnumerator Swipe_Routine(Vector3 targetPos)
+            // when it is in the correct position, the position of the other cells is checked
+            if (correctPos)
+                _cellManager.ControlCellCorrectPositions();
+        }
+        
+        public bool CalculateCorrectPosition()
+        {
+            // calculate which position it should be in according to its number from left to right
+            var column = (number - 1) % _levelManager.gridLimits.x;
+            var row = (number - 1) / _levelManager.gridLimits.x;
+            var y = _levelManager.gridLimits.y - 1 - row;
+            
+            return new Vector2Int(column, y) == gridPosition;
+        }
+        
+        public IEnumerator Swipe_Routine(Vector3 targetPos)
         {
             isTouchable = false;
             var elapsed = 0f;
-            var initPos = transform.position;
+            var initPos = transform.localPosition;
             
             while (elapsed <= _swipeTime)
             {
                 var normalized = elapsed / _swipeTime;
-                transform.position = Vector3.Lerp(initPos, targetPos, _curve.Evaluate(normalized));
+                transform.localPosition = Vector3.Lerp(initPos, targetPos, _curve.Evaluate(normalized));
                 
                 elapsed += Time.deltaTime;
                 yield return null;
             }
 
-            transform.position = targetPos;
+            transform.localPosition = targetPos;
             isTouchable = true;
         }
     }
--- Assets/_15Puzzle/Scripts/Cell/CellManager.cs
+++ Assets/_15Puzzle/Scripts/Cell/CellManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using _15Puzzle.Scripts.Level;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _15Puzzle.Scripts.Cell
 {
@@ -8,14 +11,21 @@ namespace _15Puzzle.Scripts.Cell
     {
         public List<CellController> cells;
         [SerializeField] private LevelManager _levelManager;
+        [SerializeField] private int _shuffleAnimationCount;
+        [SerializeField] private float _shuffleTime;
 
+        private void Start()
+        {
+            StartCoroutine(ShuffleAnimationForCells());
+        }
+        
         public void SwipeCells(CellController targetCell)
         {
             var targetX = targetCell.gridPosition.x;
             var targetY = targetCell.gridPosition.y;
 
             // the axes of the selected cell are checked for gap respectively
-            if (CellSwipeControl(targetX, targetY, targetX, true, true, new Vector2(-1,0)))
+            if (CellSwipeControl(targetX, targetY, targetX, true, true, new Vector2(-1, 0)))
                 return;
             if (CellSwipeControl(targetX, targetY, targetY, true, false, new Vector2(0, -1)))
                 return;
@@ -25,21 +35,22 @@ namespace _15Puzzle.Scripts.Cell
                 return;
         }
 
-        private bool CellSwipeControl(int targetX, int targetY, int targetAxis, bool isNegative, bool isXAxis, Vector2 dir)
+        private bool CellSwipeControl(int targetX, int targetY, int targetAxis, bool isNegative, bool isXAxis,
+            Vector2 dir)
         {
             // Swipe to the selected cell from the opposite direction of the cell to be swiped
 
             var findTheSpace = false;
 
             // I added different conditions to fit all directions
-            
-            var i = isNegative ? 0 : isXAxis ? _levelManager.gridLimits.x -1 : _levelManager.gridLimits.y -1; 
+
+            var i = isNegative ? 0 : isXAxis ? _levelManager.gridLimits.x - 1 : _levelManager.gridLimits.y - 1;
             for (; isNegative ? i <= targetAxis : i >= targetAxis; i += isNegative ? 1 : -1)
             {
                 // check whether the cell exists or not
-                var cell = cells.Find(cell => cell.gridPosition.x == (isXAxis ? i : targetX) 
-                                               && cell.gridPosition.y == (isXAxis ? targetY : i));
-                    
+                var cell = cells.Find(cell => cell.gridPosition.x == (isXAxis ? i : targetX)
+                                              && cell.gridPosition.y == (isXAxis ? targetY : i));
+
                 // swiping starts after the gap is found
                 if (findTheSpace && cell is not null)
                     cell.Swipe(dir);
@@ -48,6 +59,59 @@ namespace _15Puzzle.Scripts.Cell
             }
 
             return findTheSpace;
+        }
+
+        public void ControlCellCorrectPositions()
+        {
+            foreach (var cell in cells)
+            {
+                if (!cell.CalculateCorrectPosition())
+                    return;
+            }
+
+            Debug.Log("game over");
+        }
+
+        private IEnumerator ShuffleAnimationForCells()
+        {
+            // run the shuffle animation as many times as you want it to run
+            for (var i = 0; i < _shuffleAnimationCount; i++)
+            {
+                var createdPositions = new List<Vector2>();
+                
+                // each cell is assigned a random position
+                foreach (var cell in cells)
+                {
+                    // a previously uncreated position is created
+                    while (true)
+                    {
+                        var randomX = Random.Range(0, _levelManager.gridLimits.x);
+                        var randomY = Random.Range(0, _levelManager.gridLimits.y);
+                        var randomPos = new Vector3(randomX, randomY, 0);
+
+                        // returns to the beginning of the loop if a position has already been created
+                        if (createdPositions.Contains(randomPos))
+                        {
+                            yield return 0;
+                            continue;
+                        }
+
+                        StartCoroutine(cell.Swipe_Routine(randomPos));
+                        createdPositions.Add(randomPos);
+
+                        break;
+                    }
+                }
+
+                yield return new WaitForSeconds(_shuffleTime);
+            }
+
+            // They return to their original position in the level
+            foreach (var cell in cells)
+            {
+                var originalPos = new Vector3(cell.gridPosition.x, cell.gridPosition.y, 0);
+                StartCoroutine(cell.Swipe_Routine(originalPos));
+            }
         }
     }
 }
